1f46855d1358618bdeeaa1422ac416aaebac92de
Cheikh Seck <cheikh@simsol.in>
2026-02-05 14:11:23 +0000

    bypass empty script migration error

--- migrations/postgres/012_unique_user_product_active_pending.up.sql
+++ migrations/postgres/012_unique_user_product_active_pending.up.sql
@@ -7,3 +7,4 @@
 -- CREATE UNIQUE INDEX IF NOT EXISTS idx_subscriptions_user_product_active_pending
 --    ON billing.subscriptions(user_id, product_id)
 --    WHERE status IN ('active', 'pending');
+SELECT 1;
